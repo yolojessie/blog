94a0e1a30a3a46feba13e64936ed53891efc4254
yolojessie <jessie840904@gmail.com>
2018-08-02 13:52:56 +0800

    chapter 8 finished

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from article.models import Article,Comment
 # Create your views here.
 def article(request):
     '''
     Render the article page
     '''
-    return render(request, 'article/article.html')
+    articles = {}
+    for article in Article.objects.all():
+        articles.update({article:Comment.objects.filter(article=article)})
+    context = {'articles':articles}
+    
+    return render(request, 'article/article.html',context)
